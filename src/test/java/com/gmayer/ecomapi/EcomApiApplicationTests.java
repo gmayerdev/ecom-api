038a75470d13007287a16433f11b963a6bb151ff
gmayer <gmayer.dev@gmail.com>
2021-12-01 13:44:24 -0800

    Init project

---
+++ src/test/java/com/gmayer/ecomapi/EcomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmayer.ecomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
